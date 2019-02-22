19e31e1c24cbc70a4c96f4dd8f37908584391dc2
Mary Maccoun <40074843+mmaccoun@users.noreply.github.com>
2019-02-21 20:58:12 -0800

    update exon_sample.r

--- dsx_project/exon_sample.r
+++ dsx_project/exon_sample.r
@@ -1,5 +1,20 @@
-setwd("/Users/maccom/Desktop/dsx_project--master") #move all_M_data.csv and all_F_data.csv to directory to work in 
+##Mary Maccoun
+##DSX Mutation Project 
+## This file will manipulate data frame with data from ALL samples 
+##creates a new data frame with rows = exon ID and column = sample name 
+##file created 2/20/2019
+##last updated 2/21/2019
 
+
+
+
+
+library(tidyr)
+library(data.table)
+
+setwd("~/Desktop/dsx_project--master") #move all_M_data.csv and all_F_data.csv to directory to work in 
+
+######## MAKE DATA FRAME CONTAINING ALL SAMPLE DATA #############
 male <- read.csv('all_M_data.csv')
 male$sex <- "M"
 
@@ -12,10 +27,14 @@ dat$sample<- as.character(dat$sample)
 
 dat$rep <- gsub(".*_", "", dat$sample)
 dat$strain <- gsub("_.*", "", dat$sample)
-
-
-names(dat) <- c(' ', 'exonic region id', 'sample id', 'number of mapped reads', 'number of reads in the exonic region', 'coverage in exonic region (number of reads in exonic region / read length)', 'exonic region length','average per nucleotide coverage,','RPKM', 'sample','sex', 'rep', 'strain')
+names(dat) <- c( '', 'exonic region id', 'sample id', 'number of mapped reads', 'number of reads in the exonic region', 'coverage in exonic region (number of reads in exonic region / read length)', 'exonic region length','average per nucleotide coverage,','RPKM', 'sample','sex', 'rep', 'strain')
 #write.csv(dat,'all_dat.csv')
+
+####find names of sample ######
+names = data.frame(dat[, 3], dat[,10])
+
+
+
 
 # shapiro.test(log(dat$RPKM)) test to bellcurve 
 # hist(dat$RPKM) #hist of RPKM data 
@@ -23,18 +42,30 @@ names(dat) <- c(' ', 'exonic region id', 'sample id', 'number of mapped reads', 
 dat$logRPKM = log(dat$RPKM)
 
 
+####EXON by SAMPLE data frame ######## 
 
-             
-exon_sample <- data.frame(dat[ ,2], dat[,3])   
-exon_sample <- cbind(exon_sample, dat[,14]) #c1 = exon_id, c2 = sample_id, c3 = logRPKM
+exon_sample <- data.frame(dat[ ,2], dat[,3],dat[,14])   
+new_exon = unique(exon_sample) #find root error in files later alligator 
+names(new_exon) <- c('exon_id', 'sample_id', 'logRPKM')
 
-names(exon_sample) <- c('exon_id', 'sample_id', 'logRPKM')
-u <- unique(exon_sample[,1]) #search for unique exons 
-datu <- data.frame(u) #data frame with unique exons...to be removed
+ex_sam = dcast(new_exon, exon_id ~ sample_id ) #data set with row as exon ID, column is sample ID 
+#ERROR: 23 samples but 22 col :( ...problems w renaming 
 
-d <- duplicated(exon_sample[,1])
-dupd <- data.frame(d, exon_sample[,1], dat[ ,2], dat [,3]) #bool value 1, true means exon duplicated in other samples...to be analyzed later   
-names(dupd) <- c('exon_id_dups', 'sample_id', 'logRPKM')  
+
+ex_sam[ex_sam == -Inf] = NA #change -Inf to empty 
+
+missingness = apply(ex_sam[,-1], 1, function(x) sum(!is.na(x))/21) #see how many samples have exon 
+write.csv(ex_sam,'exon_sample.csv') 
+
+
+
+
+
+
+
+
+
+
 
 
 
