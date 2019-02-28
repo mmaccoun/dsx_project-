51dc094e1e794c9ccd5ad654c755f9798a81b91d
Mary Maccoun <40074843+mmaccoun@users.noreply.github.com>
2019-02-27 16:47:08 -0800

    Update analyze_exon.R

--- analyze_exon.R
+++ analyze_exon.R
@@ -7,9 +7,7 @@ data = og[, 2:24]
 
 tdata <- data.frame(t(data)) #transpose data 
 #need to separate name and rep # of data
-print(tdata[,1])
-an = tdata[(2:40189 ), ]
-dim(an)
+mat = data.matrix(tdata,rownames.force = NA)
 
 
 samplekey = data.frame(x_id = names(tdata), exon_id = og$exon_id) #to compare transposed column ID to true exon ID
@@ -23,5 +21,27 @@ sumstats = function(tdata){
   return(result) 
 }
 
-prcomp (analyze, retx = TRUE, center = TRUE, scale. = FALSE)
+prcomp_dat = prcomp (mat[-1,], retx = TRUE, center = TRUE, scale. = FALSE)
 
+str(prcomp_dat)
+prcomp_dat$x
+
+class(prcomp_dat)
+class(prcomp_dat$x)
+
+tdata$tdata[,1] <- gsub(".*_", "", tdata$exon_id)
+tdata$tdata[,1] <- gsub("..*", "", tdata$sex)
+print(tdata[1:5, 40188:40189 , 40188:40189])
+
+geno_sex = gsub("_*.\\.", "_", row.names(tdata))[-1]
+table(geno_sex)
+
+prcomp_dat$x
+
+##produce visual model of data 
+plot(prcomp_dat$x[,1], prcomp_dat$x[,2], col = as.factor(geno_sex), pch = 19,
+     xlab = "PC1 (18% var. expl.)", ylab = "PC2 (14% var.xx expl.)",
+     main = "PCA of transcriptome of control/dsx mutants")
+legend("bottomright", levels(as.factor(geno_sex)), col = c(1:6), pch = 19, cex = 0.75)
+
+pairs(prcomp_dat$x[,1:5], col = as.factor(geno_sex), pch = 19)
