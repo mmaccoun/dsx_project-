a5b081a481c6bd7e1475929967f13c5d63b8e18c
Mary Maccoun <40074843+mmaccoun@users.noreply.github.com>
2019-03-04 08:37:41 -0800

    Update analyze_exon.R

--- analyze_exon.R
+++ analyze_exon.R
@@ -1,9 +1,18 @@
+##Mary Maccoun
+##DSX Mutation Project 
+## This file analyze the logRPKM data through PCA 
+##file created 2/28/2019
+##last updated 3/4/2019
+
+library(graphics)
+
 #transpose data...exon in columns and sample name in rows 
 ##add colum for sex, strain, rep 
 
 setwd("~/Desktop/dsx_project--master")
 og = read.csv('exon_sample.csv')
-data = og[, 2:24]
+data = og[2:21]
+
 
 tdata <- data.frame(t(data)) #transpose data 
 #need to separate name and rep # of data
@@ -12,14 +21,6 @@ mat = data.matrix(tdata,rownames.force = NA)
 
 samplekey = data.frame(x_id = names(tdata), exon_id = og$exon_id) #to compare transposed column ID to true exon ID
 
-sumstats = function(tdata){
-  mean = apple(tdata, 2 ,mean)
-  median = apply(tdata, 2 , median)
-  sd = apply(tdata, 2, sd)
-  result = data.frame(mean, median, sd)
-  print(result)
-  return(result) 
-}
 
 prcomp_dat = prcomp (mat[-1,], retx = TRUE, center = TRUE, scale. = FALSE)
 
@@ -31,17 +32,13 @@ class(prcomp_dat$x)
 
 tdata$tdata[,1] <- gsub(".*_", "", tdata$exon_id)
 tdata$tdata[,1] <- gsub("..*", "", tdata$sex)
-print(tdata[1:5, 40188:40189 , 40188:40189])
+#print(tdata[1:5, 40188:40189 , 40188:40189])
 
 geno_sex = gsub("_*.\\.", "_", row.names(tdata))[-1]
 table(geno_sex)
 
+#prcopmp contains PCs for all exons 
+
 prcomp_dat$x
-
-##produce visual model of data 
-plot(prcomp_dat$x[,1], prcomp_dat$x[,2], col = as.factor(geno_sex), pch = 19,
-     xlab = "PC1 (18% var. expl.)", ylab = "PC2 (14% var.xx expl.)",
-     main = "PCA of transcriptome of control/dsx mutants")
-legend("bottomright", levels(as.factor(geno_sex)), col = c(1:6), pch = 19, cex = 0.75)
-
-pairs(prcomp_dat$x[,1:5], col = as.factor(geno_sex), pch = 19)
+print(prcomp_dat$x)
+write.csv(prcomp_dat$x, "prcomp_data.csv") #save pcomp data to CSV file 
