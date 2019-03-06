6af1dde80f68f5b83a63a301679b9248c4b30a20
Mary Maccoun <40074843+mmaccoun@users.noreply.github.com>
2019-03-05 16:24:03 -0800

    Update visualize_PCA.R

--- visualize_PCA.R
+++ visualize_PCA.R
@@ -2,7 +2,7 @@
 ##DSX Mutation Project 
 ## This file will plot the PCA data from the samples 
 ##file created 3/4/2019
-##last updated 3/5/2019
+##last updated 3/4/2019
 
 library(ggplot2)
 
@@ -16,7 +16,11 @@ cbind(data,strain)
 cbind(data,rep)
 print(data)
 
-ggplot(data, aes(PC1, PC2, color = strain, fill = strain)) + geom_point(aes(shape = sex)) + scale_shape_manual(values = c(10,3))+ stat_ellipse(type = "norm", geom = "polygon", alpha = 0.35, level = 0.5,aes(fill = strain)) + labs(x = "PC1 (20.2% var. explained)", y = "PC2 (15.2% var. explained)")
+ggplot(data, aes(PC1, PC2, color = strain, fill = strain)) + geom_point(aes(shape = sex)) + scale_shape_manual(values = c(10,3))+ stat_ellipse(type = "norm", geom = "polygon", alpha = 0.35, level = 0.5,aes(fill = strain)) + labs(x = "PC1 (20.2% var. explained)", y = "PC2 (15.2% var. explained)")+ ggtitle("PCA of Transcriptome of Control/Dsx Mutant")
 
 
 
+                                                                       
+##label X and Y axis 
+
+## fix bug: too few points to calc an ellipse 
