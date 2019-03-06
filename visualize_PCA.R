94f22aa9369c69f7b036bd1a2b5963b1bd9c751e
Mary Maccoun <40074843+mmaccoun@users.noreply.github.com>
2019-03-05 16:17:47 -0800

    Update visualize_PCA.R

--- visualize_PCA.R
+++ visualize_PCA.R
@@ -2,7 +2,7 @@
 ##DSX Mutation Project 
 ## This file will plot the PCA data from the samples 
 ##file created 3/4/2019
-##last updated 3/4/2019
+##last updated 3/5/2019
 
 library(ggplot2)
 
@@ -13,10 +13,10 @@ sex = c("XX", "XY", "XX", "XY", "XX", "XY", "XX", "XY", "XX", "XY", "XX", "XY", 
 cbind(data,sex)
 strain = c("berlin", "berlin", "berlin", "berlin", "berlin", "berlin", "canton", "canton","canton","canton","canton","canton","canton","dsx", "dsx","dsx","dsx","dsx","dsx" )
 cbind(data,strain)
+cbind(data,rep)
 print(data)
 
-ggplot(data, aes(PC1,PC2)) + geom_point(aes(shape = group)) + geom_text(aes(label = data$X)) + stat_ellipse(aes(fill=strain))
+ggplot(data, aes(PC1, PC2, color = strain, fill = strain)) + geom_point(aes(shape = sex)) + scale_shape_manual(values = c(10,3))+ stat_ellipse(type = "norm", geom = "polygon", alpha = 0.35, level = 0.5,aes(fill = strain)) + labs(x = "PC1 (20.2% var. explained)", y = "PC2 (15.2% var. explained)")
 
-#currently different plot points for XX and XY
-#elipses grouped by strain 
-##how to improve the plot--currently unclear 
+
+
